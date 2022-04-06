b440e75c6159dc81d1704458f1a578f6c69cfa0f
Giuseppe Pennisi <giuseppe.pennisi@rbtech.dev>
2022-04-06 17:13:10 +0200

    chore: project cleaned

--- jest.preset.js
+++ jest.preset.js
@@ -1,14 +1,7 @@
 const nxPreset = require('@nrwl/jest/preset');
-const esModules = [
-  'lodash-es',
-  '@fortawesome/angular-fontawesome',
-  '.*\\.mjs$',
-].join('|');
-
 module.exports = {
   ...nxPreset,
   moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
-  transformIgnorePatterns: [`/node_modules/(?!${esModules}/)`],
   transform: {
     '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular',
   },
